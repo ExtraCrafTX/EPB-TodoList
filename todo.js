5b056b770fe7064b55d78e9d09ef5f08f5f58ca9
Kaamil Jasani <kaamilj@yahoo.com>
2020-10-25 18:02:33 +0000

    Initial todo list

---
+++ todo.js
@@ -0,0 +1,27 @@
+$(function(){
+    $("#add").submit(event => {
+        event.preventDefault();
+        console.log(event);
+        console.log(event.target);
+        let form = $(event.target);
+        let item = form.children("input").val();
+        let li = $("<li></li>");
+
+        li.append("<input type='checkbox' />");
+        
+        let span = $("<span></span>");
+        span.text(item);
+        li.append(span);
+        
+        let button = $("<button>Delete</button>");
+        button.click(event => {
+            // console.log("deleting");
+            console.log(event.target);
+            $(event.target).parent().remove();
+        });
+        li.append(button);
+        
+        $("#list").append(li);
+        form.children("input").val("");
+    });
+});
