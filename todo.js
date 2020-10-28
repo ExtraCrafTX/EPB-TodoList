1d6d06f7832d3dac60e3a4463332ddaf473462c9
Kaamil Jasani <kaamilj@yahoo.com>
2020-10-28 21:32:55 +0000

    Introduce Bootstrap

--- todo.js
+++ todo.js
@@ -4,24 +4,30 @@ $(function(){
         console.log(event);
         console.log(event.target);
         let form = $(event.target);
-        let item = form.children("input").val();
-        let li = $("<li></li>");
+        let item = $("#adding").val();
+        console.log(item);
+        let li = $("<li class='row'></li>");
+        let col1 = $("<div class='col'></div>");
+        li.append(col1);
 
-        li.append("<input type='checkbox' />");
+        col1.append("<input type='checkbox' class='mr-2 mr-lg-3' />");
         
         let span = $("<span></span>");
         span.text(item);
-        li.append(span);
+        col1.append(span);
+
+        let col2 = $("<div class='col-2'></div>");
+        li.append(col2);
         
-        let button = $("<button class='delete'>Delete</button>");
+        let button = $("<button class='delete w-100'>Delete</button>");
         button.click(event => {
             // console.log("deleting");
             console.log(event.target);
-            $(event.target).parent().remove();
+            $(event.target).parent().parent().remove();
         });
-        li.append(button);
+        col2.append(button);
         
         $("#list").append(li);
-        form.children("input").val("");
+        $("#adding").val("");
     });
 });
